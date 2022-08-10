fbba79d0790d606f43d18eb0cd211958e53231a3
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2022-08-10 09:20:31 -0300

    Piscando fileira de leds

---
+++ Leds/Leds.ino
@@ -0,0 +1,25 @@
+int led[] = {12, 11, 10, 9, 8, 7}; //definindo os pinos do led em forma de matriz
+
+void setup()
+{
+    pinMode(led[0], OUTPUT);// definindo modo do pino dos leds
+    pinMode(led[1], OUTPUT);// definindo modo do pino dos leds
+    pinMode(led[2], OUTPUT);// definindo modo do pino dos leds
+    pinMode(led[3], OUTPUT);// definindo modo do pino dos leds
+    pinMode(led[4], OUTPUT);// definindo modo do pino dos leds
+    pinMode(led[5], OUTPUT);// definindo modo do pino dos leds
+}
+
+void loop()
+{
+
+    for (byte x = 0; x < 6; x++)
+    {
+        //Piscando o led de acorcom a posição x na matriz
+        //usado for para realizar a variação de x sem a necessidade de repetir o código
+        digitalWrite(led[x], HIGH);
+        delay(100);
+         digitalWrite(led[x], LOW);
+        delay(100);
+    }
+}
